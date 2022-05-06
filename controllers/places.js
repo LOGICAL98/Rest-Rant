69a0c7519b01ae0a4b48e74c283a7e4813bf57fb
LOGICAL98 <67430411+LOGICAL98@users.noreply.github.com>
2022-05-06 14:35:44 -0400

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -12,18 +12,19 @@ router.get('/new', (req, res) => {
 
 
 router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+  if (!req.body.pic){
+    req.body.pic = 'public/images/abstract.jpg'
   }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
+  if (!req.body.city){
+    req.body.city='Anytown'
   }
   if (!req.body.state) {
-    req.body.state = 'USA'
+    req.body.state ='USA'
   }
   places.push(req.body)
   res.redirect('/places')
 })
+
+
 
 module.exports = router
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,28 +6,28 @@ function new_form() {
         <Def>
             <main>
                 <h1>Add a New Place</h1>
-                <form method = "POST" action= "/places">
+                <form method="POST" action="/places">
                     <div className="form-group">
                         <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" required />
+                        <input className="form-control" id="name" name="name" required></input>
                     </div>
                     <div className="form-group">
-                        <label htmlFor ="pic">Place Picture</label>
-                        <input className="form-control" type="url" id="pic" name="pic" />
+                        <label htmlFor="pic"> Place Pictures</label>
+                        <input className="form-control" type="url" id="pic" name="pic"></input>
                     </div>
                     <div className="form-group">
                         <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" />
+                        <input className="form-control" id="city" name="city"></input>
                     </div>
                     <div className="form-group">
                         <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" />
+                        <input className="form-control" id="state" name="state"></input>
                     </div>
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" required />
+                        <input className="form-control" id="cuisines" name="cuisines" required></input>
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                    <input className="btn btn-primary" type="submit" value="Add Place"></input>
                 </form>
             </main>
         </Def>
